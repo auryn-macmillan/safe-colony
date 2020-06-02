f883f8580f0e74a972b9ca42dd8a3133abb54d0d
Auryn Macmillan <aurynmacmillan@gmail.com>
2020-06-02 13:13:24 -0400

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-layout view="lHr lpr fFf">
+
+    <q-header class="bg-primary text-white">
+
+      <q-tabs align="left">
+        <q-route-tab to="/" label="Tokens" />
+        <q-route-tab to="/" label="Permissions" />
+        <q-route-tab to="/" label="Rewards" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer
+           v-model="drawer"
+           show-if-above
+           :width="150"
+           :breakpoint="600"
+         >
+           <q-scroll-area style="height: calc(100% - 150px); margin-top: 48px">
+             <q-list padding>
+               <q-item clickable v-ripple>
+                 <q-item-section>
+                   All Domains
+                 </q-item-section>
+               </q-item>
+
+               <q-item active clickable v-ripple>
+                 <q-item-section>
+                   Rewards
+                 </q-item-section>
+               </q-item>
+
+               <q-item clickable v-ripple>
+                 <q-item-section>
+                   Root
+                 </q-item-section>
+               </q-item>
+
+               <q-item clickable v-ripple>
+                 <q-item-section>
+                   Domain 1
+                 </q-item-section>
+               </q-item>
+
+               <q-item clickable v-ripple>
+                 <q-item-section>
+                   Domain 2
+                 </q-item-section>
+               </q-item>
+             </q-list>
+           </q-scroll-area>
+         </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      left: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Safe Colony"
+      src="statics/logomark_white.svg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
